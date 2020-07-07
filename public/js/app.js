352048519b80a9e280b3503655a6c5b4b887939f
lltonll <lltonll@MacBook-Pro.local>
2020-07-07 23:29:11 +0700

    add placeName object to the result

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -21,6 +22,7 @@ weatherForm.addEventListener('submit', (e) => {
     // console.log(location)
     messageOne.textContent = 'Loading'
     messageTwo.textContent = ''
+    messageThree.textContent = ''
     // http://localhost:3000/weather?address='+encodeURIComponent(location) this is before heroku
     fetch('/weather?address='+encodeURIComponent(location)).then((response)=> {
         // console.log(encodeURIComponent(location))
@@ -32,6 +34,8 @@ weatherForm.addEventListener('submit', (e) => {
         } else {
             messageOne.textContent = data.temperature
             messageTwo.textContent = data.feelslike
+            messageThree.textContent = data.PlaceName
+
             // console.log(data.temperature)
             // console.log(data.feelslike)
         } 
