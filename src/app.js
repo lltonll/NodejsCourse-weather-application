4dfb692364a4100ac8819ea9961c02f4b7d18216
Piyawat Thamthada <lltonll@Piyawats-MacBook-Pro.local>
2020-08-10 14:03:29 +0700

    change to check render function

--- src/app.js
+++ src/app.js
@@ -113,7 +113,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Tony Piyawat',
+        name: 'ssss',
         errorMessage: 'Page not found!'
     })
 })
